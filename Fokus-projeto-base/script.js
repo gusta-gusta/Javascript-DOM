9c90a7bd7dbd4436b650f404ea90d35771b4b1fb
Gustavo Sales Rodrigues <gustavo_sales2014@outlook.com>
2025-08-29 11:49:47 -0300

    Primeiros Passos
    criando ação ao botão

---
+++ Fokus-projeto-base/script.js
@@ -0,0 +1,24 @@
+const html = document.querySelector('html')
+const botaoFoco = document.querySelector('.app__card-button--foco')
+const botaoCurto = document.querySelector('.app__card-button--curto')
+const botaoLongo = document.querySelector('.app__card-button--longo')
+
+botaoFoco.addEventListener('click', () => {
+
+    html.setAttribute('data-contexto', 'foco')
+
+})
+
+
+botaoCurto.addEventListener('click', () => {
+
+    html.setAttribute('data-contexto', 'descanso-curto')
+
+})
+
+
+botaoLongo.addEventListener('click', () => {
+
+    html.setAttribute('data-contexto', 'descanso-longo')
+
+})
